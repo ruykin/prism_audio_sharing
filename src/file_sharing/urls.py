b66fb79d6cb8e6459e6f5748c333275ce6e26c50
Siddhant Shrivastav <siddhantshrivastava@outlook.com>
2020-04-11 16:20:15 +0530

    Added download feature.

--- src/file_sharing/urls.py
+++ src/file_sharing/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('upload/', views.upload, name='upload'),
     path('friends/<int:audio_id>/', views.friends, name='friends'),
     path('share/<int:audio_id>/<int:user_id>', views.share, name='share'),
+    path('download/<int:audio_id>/', views.download, name='download'),
 ]
--- src/file_sharing/views.py
+++ src/file_sharing/views.py
@@ -1,9 +1,14 @@
-from os import path, access, R_OK
+import mimetypes
+from os import path, access, R_OK, stat
+from wsgiref.util import FileWrapper
+
 from django.core import serializers
 from django.contrib.auth.models import User
 import json
 from django.db.models import Q
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from django.utils.encoding import smart_str
 from scipy.io.wavfile import read, write
 from numpy import array
 
@@ -25,6 +30,15 @@ def share(request, *args, **kwargs):
     share_to = User.objects.get(id=kwargs['user_id'])
     SharedFiles.objects.create(shared_by=share_by, shared_to=share_to, uploaded_audio=uploaded_audio)
     return redirect('home')
+
+
+def download(request, *args, **kwargs):
+    uploaded_audio = UploadedAudio.objects.get(id=kwargs['audio_id'])
+    file_path = 'media/{}'.format(str(uploaded_audio.document))
+    with open(file_path, 'rb') as fh:
+        response = HttpResponse(fh.read(), content_type="audio/wave")
+        response['Content-Disposition'] = 'inline; filename=' + path.basename(file_path)
+        return response
 
 
 def upload(request):
@@ -49,7 +63,7 @@ def upload(request):
                 i[0], i[1] = i[1], i[0] + 2000000000
                 # to de-encrypt
                 # i[1], i[0] = i[0], i[1] - 2000000000
-            write('media/documents/'+tail, sample_rate, d)
+            write('media/documents/' + tail, sample_rate, d)
             return redirect('home')
     else:
         form = UploadedAudioForm()
